dccc73a5d8979f4b7a98052e539bb73f84fbec73
Amalia Hay <amaliawhay@gmail.com>
2020-08-06 18:55:20 -0700

    Making sure files are up to date

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (255) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY
+    (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', TRUE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Double Double', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('California Burger', TRUE);
