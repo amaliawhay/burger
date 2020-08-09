06007a9c44dcaf1640697f2b41cd061f7a6394ef
Amalia Hay <amaliawhay@gmail.com>
2020-08-09 14:48:50 -0700

    added all, create and update functions to burger object. Export to burgers controller

--- models/burgers.js
+++ models/burgers.js
@@ -1,7 +1,26 @@
-const ORM = require("../config/orm.js");
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-const burger;
-//Code to call ORM functions
+var burger = {
+  all: function (cb) {
+    orm.all("burger", function (res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function (cols, vals, cb) {
+    orm.create("burger", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function (
+      res
+    ) {
+      cb(res);
+    });
+  },
+};
 
-//exports burger file
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
